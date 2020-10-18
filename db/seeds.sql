b7a96487f77959d9f3b528250be14d8453b9ae8f
8bitk9 <67807672+dallisconner@users.noreply.github.com>
2020-10-17 23:27:49 -0400

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('The Meeseeks');
+INSERT INTO burgers (name) VALUES ('The Bobbi');
+INSERT INTO burgers (name) VALUES ('The Clive Owen');
+INSERT INTO burgers (name, eaten) VALUES ('The Sylvester', true);
+INSERT INTO burgers (name, eaten) VALUES ('The Marilyn Mansion of Marilyn Manson', true);
+INSERT INTO burgers (name, eaten) VALUES ('The Joe Biden', true);
