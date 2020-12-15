94ac69e5ad060f45758a5c33967a8f0931435ad6
8bitk9 <67807672+dallisconner@users.noreply.github.com>
2020-12-15 16:15:42 -0500

    Changes made for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+var connection;
+
+if (process.env.JAWSBD_URL) {
+  connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burger_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
